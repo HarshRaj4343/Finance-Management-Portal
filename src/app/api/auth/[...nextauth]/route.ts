7fb731133bfaddca52f64a873440c72a6af7ab03
YashSharma-code <yash.sh.aqua@gmail.com>
2026-08-13 15:46:02 +0530

    Update finance app

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,11 @@ const ldapConfig = {
 
   baseDN:process.env.BaseDN,
   
+<<<<<<< HEAD
   ou: process.env.OU ? JSON.parse(process.env.OU) : ["students_ug", "Faculty", "Staff"],
+=======
+  ou: process.env.OU ? JSON.parse(process.env.OU) : ["students_ug", "Faculty", "Staff"],
+>>>>>>> 89ce632 (Update finance app)
 };
 
 // ---------------- LDAP Authentication ----------------
